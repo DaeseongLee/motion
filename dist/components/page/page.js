f0c5a3b805ca997b25339e729ae8794565343452
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-04-09 16:34:51 +0900

    Add removeFrom in Component

--- dist/components/page/page.js
+++ dist/components/page/page.js
@@ -7,10 +7,18 @@ class PageItemComponent extends BaseComponent {
                         <button class="close">&times;</button>
                     </div>
                 </li`);
+        const closeBtn = this.element.querySelector('.close');
+        closeBtn.onclick = () => {
+            var _a;
+            (_a = this.closeListener) === null || _a === void 0 ? void 0 : _a.call(this);
+        };
     }
     addChild(child) {
         const container = this.element.querySelector('.page-item__body');
         child.attachTo(container);
+    }
+    setOnCloseListener(listener) {
+        this.closeListener = listener;
     }
 }
 export class PageComponent extends BaseComponent {
@@ -21,5 +29,8 @@ export class PageComponent extends BaseComponent {
         const item = new PageItemComponent();
         item.addChild(section);
         item.attachTo(this.element, 'beforeend');
+        item.setOnCloseListener(() => {
+            item.removeFrom(this.element);
+        });
     }
 }
